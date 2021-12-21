f2437dcabb82e035a0541a4e261e2209a164ac94
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-12-21 14:40:55 +0100

    dashboard now shows messages that are in the database.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const Message = require('../models/message');
+const async = require('async');
+const user = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Message.find().exec(function(err, messages){
+  async.parallel({
+    messages: function(callback) {
+      Message.find().populate('author').exec(callback);
+    },
+    users: function(callback){
+      user.find().exec(callback);
+    }
+  }, function(err, results){
     if(err) {
       next(err)
     }
-    res.render('index', { title: 'Dashboard', messages: messages });
-  })
-  
+    res.render('index', { title: 'Dashboard', messages: results.messages, user: false });
+  });
 });
 
 module.exports = router;
